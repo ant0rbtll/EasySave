83b64bde99e342c86d54b15bfddba6fd114b9929
Gosyfrone <alexandre.rivet.hermantier@gmail.com>
2026-01-28 16:10:27 +0100

    add : unit tests

---
+++ tests/EasySave.Backup.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EasySave.Backup.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/EasySave.State.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EasySave.State.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
