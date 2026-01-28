5600774c58d9c7d115293996d3ad48795f1f5f6a
Gosyfrone <alexandre.rivet.hermantier@gmail.com>
2026-01-28 16:38:24 +0100

    feat : recursive fil traversal

--- src/EasySave.Backup/BackupEngine.cs
+++ src/EasySave.Backup/BackupEngine.cs
@@ -26,7 +26,14 @@ public class BackupEngine
 
     public void Execute(SaveWork job)
     {
-        // To be implemented
+        if (job.Type == BackupType.Complete)
+        {
+            ExecuteCompleteBackup(job);
+        }
+        else
+        {
+            ExecuteDifferentialBackup(job);
+        }
     }
 
     private void ExecuteCompleteBackup(SaveWork job)
@@ -39,7 +46,15 @@ public class BackupEngine
 
     private IEnumerable<string> GetAllFiles(string source)
     {
-        return Enumerable.Empty<string>();
+        var files = _fileSystem.EnumerateFiles(source);
+
+        foreach (var directory in _fileSystem.EnumerateDirectories(source))
+        {
+            files = files.Concat(GetAllFiles(directory));
+        }
+
+        return files;
     }
+
 
 }
