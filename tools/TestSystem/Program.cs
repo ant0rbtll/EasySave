0cfad8a6138583e10c6ad84d7ebb1e534c3a4900
Youcef Afane <youcefafane@MacBook-Air-de-Youcef.local>
2026-01-29 11:04:59 +0100

    Add TestSystem tool to validate EasySave.System transfers

--- tools/TestSystem/Program.cs
+++ tools/TestSystem/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EasySave.System;
+
+IFileSystem fs = new DefaultFileSystem();
+ITransferService ts = new DefaultTransferService(fs);
+
+var src = Path.Combine(Path.GetTempPath(), "src.txt");
+var dst = Path.Combine(Path.GetTempPath(), "dst.txt");
+
+File.WriteAllText(src, "hello");
+var r = ts.TransferFile(src, dst, true);
+
+Console.WriteLine($"{r.IsSuccess} {r.FileSizeBytes} {r.TransferTimeMs} {r.ErrorCode}");
