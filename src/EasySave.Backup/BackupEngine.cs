9b38ffa62b4a50339ab3a14a960a63d364f22eaa
Gosyfrone <alexandre.rivet.hermantier@gmail.com>
2026-01-30 14:04:23 +0100

    feat : StateEntry

--- src/EasySave.Backup/BackupEngine.cs
+++ src/EasySave.Backup/BackupEngine.cs
@@ -26,76 +26,100 @@ public class BackupEngine
 
     public void Execute(SaveWork job)
     {
-        if (job.Type == BackupType.Complete)
+        var files = GetAllFiles(job.Source).ToList();
+
+        int totalFiles = files.Count;
+        long totalSize = files.Sum(f => _fileSystem.GetFileSize(f));
+
+        int remainingFiles = totalFiles;
+        long remainingSize = totalSize;
+
+        UpdateState(job, BackupStatus.Active, totalFiles, totalSize, remainingFiles, remainingSize, 0, "", "");
+
+        foreach (var file in files)
         {
-            ExecuteCompleteBackup(job);
+            var relativePath = Path.GetRelativePath(job.Source, file);
+            var destinationFile = Path.Combine(job.Destination, relativePath);
+
+            if (CanCopyFile(job.Type, file, destinationFile))
+            {
+                var destinationDir = Path.GetDirectoryName(destinationFile)!;
+
+                if (!_fileSystem.DirectoryExists(destinationDir))
+                {
+                    _fileSystem.CreateDirectory(destinationDir);
+                }
+
+                TransferResult result = _transferService.TransferFile(file, destinationFile);
+
+                remainingFiles--;
+                remainingSize -= result.FileSizeBytes;
+
+                int progress = (int)(
+                    100.0 * (totalFiles - remainingFiles) / totalFiles
+                );
+
+                UpdateState(job, BackupStatus.Active, totalFiles, totalSize, remainingFiles, remainingSize, progress, file, destinationFile);
+            }
         }
-        else if (job.Type == BackupType.Differential)
+
+        UpdateState(job, BackupStatus.Done, totalFiles, totalSize, 0, 0, 100, "", "");
+    }
+    private bool CanCopyFile(BackupType type, string sourceFile, string destinationFile)
+    {
+        if (type == BackupType.Complete)
         {
-            ExecuteDifferentialBackup(job);
+            return true;
+        }
+        else if (type == BackupType.Differential)
+        {
+            var destinationDir = Path.GetDirectoryName(destinationFile)!;
+            if (!_fileSystem.DirectoryExists(destinationDir))
+            {
+                return true;
+            }
+            if (!_fileSystem.FileExists(destinationFile))
+            {
+                return true;
+            }
+
+            var sourceSize = _fileSystem.GetFileSize(sourceFile);
+            var destSize = _fileSystem.GetFileSize(destinationFile);
+
+            return sourceSize != destSize;
         }
         else
         {
-            // TODO: Throw exception
+            throw new NotSupportedException($"Backup type {type} is not supported.");
         }
     }
 
-    private void ExecuteCompleteBackup(SaveWork job)
-    {
-        var files = GetAllFiles(job.Source);
+    private void UpdateState(
+    SaveWork job,
+    BackupStatus status,
+    int totalFiles,
+    long totalSize,
+    int remainingFiles,
+    long remainingSize,
+    int progress,
+    string src,
+    string dst)
+{
+    _stateWriter.Update(new StateEntry {
+        backupId = job.Id,
+        backupName = job.Name,
+        timestamp = DateTime.Now,
+        status = status,
+        totalFiles = totalFiles,
+        totalSizeBytes = totalSize,
+        remainingFiles = remainingFiles,
+        remainingSizeBytes = remainingSize,
+        progressPercent = progress,
+        currentSourcePath = src,
+        currentDestinationPath = dst
+    });
+}
 
-        foreach (var file in files)
-        {
-            var relativePath = Path.GetRelativePath(job.Source, file);
-            var destinationFile = Path.Combine(job.Destination, relativePath);
-            var destinationDir = Path.GetDirectoryName(destinationFile)!;
-
-            if (!_fileSystem.DirectoryExists(destinationDir))
-            {
-                _fileSystem.CreateDirectory(destinationDir);
-            }
-
-            _transferService.TransferFile(file, destinationFile);
-        }
-    }
-
-    private void ExecuteDifferentialBackup(SaveWork job)
-    {
-        var files = GetAllFiles(job.Source);
-
-        foreach (var file in files)
-        {
-            var relativePath = Path.GetRelativePath(job.Source, file);
-            var destinationFile = Path.Combine(job.Destination, relativePath);
-
-            bool shouldCopy = false;
-
-            if (!_fileSystem.DirectoryExists(Path.GetDirectoryName(destinationFile)!))
-            {
-                _fileSystem.CreateDirectory(Path.GetDirectoryName(destinationFile)!);
-                shouldCopy = true;
-            }
-            else if (!_fileSystem.FileExists(destinationFile))
-            {
-                shouldCopy = true;
-            }
-            else
-            {
-                var sourceSize = _fileSystem.GetFileSize(file);
-                var destSize = _fileSystem.GetFileSize(destinationFile);
-
-                if (sourceSize != destSize)
-                {
-                    shouldCopy = true;
-                }
-            }
-
-            if (shouldCopy)
-            {
-                _transferService.TransferFile(file, destinationFile);
-            }
-        }
-    }
 
     private IEnumerable<string> GetAllFiles(string source)
     {
