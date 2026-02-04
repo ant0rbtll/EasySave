06cd25277374f6c3daf84eafbfea37bb789a5275
Gosyfrone <alexandre.rivet.hermantier@gmail.com>
2026-02-04 02:13:36 +0100

    fix : ETR

--- src/EasySave.State/StateEntry.cs
+++ src/EasySave.State/StateEntry.cs
@@ -2,15 +2,15 @@ namespace EasySave.State;
 
 public class StateEntry
 {
-    public int BackupId;
-    public string? BackupName;
-    public DateTime Timestamp;
-    public BackupStatus Status;
-    public int TotalFiles;
-    public long TotalSizeBytes;
-    public int RemainingFiles;
-    public long RemainingSizeBytes;
-    public int ProgressPercent;
-    public string? CurrentSourcePath;
-    public string? CurrentDestinationPath;
+    public int BackupId { get; set; }
+    public string? BackupName { get; set; }
+    public DateTime Timestamp { get; set; }
+    public BackupStatus Status { get; set; }
+    public int TotalFiles { get; set; }
+    public long TotalSizeBytes { get; set; }
+    public int RemainingFiles { get; set; }
+    public long RemainingSizeBytes { get; set; }
+    public int ProgressPercent { get; set; }
+    public string? CurrentSourcePath { get; set; }
+    public string? CurrentDestinationPath { get; set; }
 }
