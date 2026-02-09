345f0d2bdc4b9e61eb3f3705d61ab09b4895f3d9
Antonin RABATEL <arabatel34@gmail.com>
2026-02-09 15:26:07 +0100

    fix: supression de la progression

--- src/EasySave.Localization/LocalizationKey.cs
+++ src/EasySave.Localization/LocalizationKey.cs
@@ -82,7 +82,6 @@ public enum LocalizationKey
     // GUI
     gui_sidebar_create,
     gui_sidebar_manage,
-    gui_sidebar_progress,
     gui_sidebar_log,
     gui_sidebar_config,
     gui_config_title,
