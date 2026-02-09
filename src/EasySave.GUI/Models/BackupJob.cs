48937987d9d2b7027bb969d6a23d01a7373d5ada
TouzeR <romain.touze@etu.umontpellier.fr>
2026-02-09 11:34:44 +0100

    feat : finish model backupJob

--- src/EasySave.GUI/Models/BackupJob.cs
+++ src/EasySave.GUI/Models/BackupJob.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasySave.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,54 @@ namespace EasySave.GUI.Models
 {
     public class BackupJob : IModel
     {
-		private string name;
+        public int Id { get; set; }
+
+        private string name;
 
 		public string Name
 		{
 			get { return name; }
-			set { name = value; OnNotifyPropertyChanged("Name"); }
+			set { name = value; OnNotifyPropertyChanged(nameof(Name)); }
 		}
 
+		private string source;
+
+		public string Source
+		{
+			get { return source; }
+			set { source = value; OnNotifyPropertyChanged(nameof(Source)); }
+		}
+
+		private string destination;
+
+		public string Destination
+		{
+			get { return destination; }
+			set { destination = value; OnNotifyPropertyChanged(nameof(Destination)); }
+		}
+
+		private BackupType type;
+
+		public BackupType Type
+		{
+			get { return type; }
+			set { type = value; OnNotifyPropertyChanged(nameof(Type)); }
+		}
+
+		private DateTime? latestExecutionDate = null;
+
+		public DateTime? LastExecutionDate
+		{
+			get { return latestExecutionDate; }
+			set { latestExecutionDate = value; OnNotifyPropertyChanged(nameof(LastExecutionDate)); } 
+		}
+
+		private bool isActive = false;
+
+		public bool IsActive
+		{
+			get { return isActive; }
+			set { isActive = value; OnNotifyPropertyChanged(nameof(IsActive)); }
+		}
 	}
 }
