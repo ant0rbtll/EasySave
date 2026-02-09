25250e3f76544a3997d8cb51f4e7b6097ed1c6b4
ant0rbtll <arabatel34@gmail.com>
2026-02-09 11:44:01 +0100

    Ajout du squelette vierge (#102)

--- src/EasySave.GUI/ViewModels/MainWindowViewModel.cs
+++ src/EasySave.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace EasySave.GUI.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EasySave.GUI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public SidebarViewModel Sidebar { get; }
+
+    // La page courante affichée dans ContentControl
+    [ObservableProperty]
+    private ViewModelBase currentPage;
+
+    // ViewModels des différentes pages
+    private readonly CreateViewModel _createViewModel;
+    private readonly ManageViewModel _manageViewModel;
+    private readonly ProgressViewModel _progressViewModel;
+    private readonly LogViewModel _logViewModel;
+    private readonly ConfigViewModel _configViewModel;
+
+    public MainWindowViewModel()
+    {
+        // Initialisation des ViewModels
+        _createViewModel = new CreateViewModel();
+        _manageViewModel = new ManageViewModel();
+        _progressViewModel = new ProgressViewModel();
+        _logViewModel = new LogViewModel();
+        _configViewModel = new ConfigViewModel();
+
+        Sidebar = new SidebarViewModel(Navigate);
+        currentPage = _createViewModel; // page par défaut
+    }
+
+    public void Navigate(string page)
+    {
+        CurrentPage = page switch
+        {
+            "creation" => _createViewModel,
+            "manage" => _manageViewModel,
+            "progress" => _progressViewModel,
+            "log" => _logViewModel,
+            "conf" => _configViewModel,
+            _ => CurrentPage
+        };
+    }
 }
