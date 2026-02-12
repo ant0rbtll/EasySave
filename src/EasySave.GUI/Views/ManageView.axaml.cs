e6efc3e31a87a70b628f5398a9beb5e5b8b4ff8c
ant0rbtll <arabatel34@gmail.com>
2026-02-12 11:41:19 +0100

    Implémentation de l'édition d'un backupjob dans la vue manage (#142)

--- src/EasySave.GUI/Views/ManageView.axaml.cs
+++ src/EasySave.GUI/Views/ManageView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using EasySave.GUI.ViewModels;
 
 namespace EasySave.GUI.Views;
 
@@ -7,5 +8,47 @@ public partial class ManageView : UserControl
     public ManageView()
     {
         InitializeComponent();
+    }
+
+    private async void EditBrowseSource_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is not ManageViewModel mvm || mvm.EditingJob is not { } vm)
+            return;
+
+        var dialog = new OpenFolderDialog { Title = vm.BrowseSourceTitle };
+        var result = await dialog.ShowAsync((Window)this.VisualRoot!);
+
+        if (!string.IsNullOrWhiteSpace(result))
+            vm.SetSourcePathFromDialog(result);
+    }
+
+    private async void EditBrowseDestination_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is not ManageViewModel mvm || mvm.EditingJob is not { } vm)
+            return;
+
+        var dialog = new OpenFolderDialog { Title = vm.BrowseDestinationTitle };
+        var result = await dialog.ShowAsync((Window)this.VisualRoot!);
+
+        if (!string.IsNullOrWhiteSpace(result))
+            vm.SetDestinationPathFromDialog(result);
+    }
+
+    private void EditName_LostFocus(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is ManageViewModel mvm && mvm.EditingJob is { } vm)
+            vm.ValidateNameOnLostFocus();
+    }
+
+    private void EditSource_LostFocus(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is ManageViewModel mvm && mvm.EditingJob is { } vm)
+            vm.ValidateSourcePathOnLostFocus();
+    }
+
+    private void EditDestination_LostFocus(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (DataContext is ManageViewModel mvm && mvm.EditingJob is { } vm)
+            vm.ValidateDestinationPathOnLostFocus();
     }
 }
