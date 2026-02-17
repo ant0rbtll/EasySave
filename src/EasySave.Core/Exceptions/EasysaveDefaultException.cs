9e2a98ae56eb4dd7e939f704998203af79ed277c
TouzeR <romain.touze@etu.umontpellier.fr>
2026-02-17 10:39:02 +0100

    feat init exceptions

---
+++ src/EasySave.Core/Exceptions/EasysaveDefaultException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Core.Exceptions
+{
+    public class EasysaveDefaultException : SystemException
+    {
+        protected string _errorKey = string.Empty;
+        protected int _errorCode = 0;
+        protected string _message = string.Empty;
+        protected string[] _options = [];
+
+        public EasysaveDefaultException(string message,string errorKey, int errorCode ,string[] options)
+        {
+            _errorKey = errorKey;
+            _errorCode = errorCode;
+           _message = message;
+            _options = options;
+
+        }
+
+        public string getTranslatedTexte()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/EasySave.Core/Exceptions/JobNamedTestException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Core.Exceptions
+{
+    public class JobNamedTestException : EasysaveDefaultException
+    {
+        public JobNamedTestException(
+            string[] options
+            ) 
+            : base("Le job doit pas s'appeller {0}",
+                  "job_name_test",
+                  200,
+                  options)
+        {
+
+        }
+    }
+}
